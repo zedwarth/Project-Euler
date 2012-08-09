4ccfc647baf0fbd787767e2663ca07c7e0c1aeb0
zedwarth <zedwarth@gmail.com>
2012-08-09 00:17:47 -0400

    added problem 6

--- problem-6/6.py
+++ problem-6/6.py
@@ -6,3 +6,17 @@
 #(1 + 2 + ... + 10)2 = 552 = 3025
 #Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+import math
+#Find the sum of the squares of the first one hundred nautral numbers
+a = 0
+for x in range(1, 101):
+    a += x ** 2
+#Find the square of the sum of the first one hundred natural numbers
+b = 0
+for x in range(1, 101):
+    b += x
+b = b ** 2
+
+#Find the difference between the sum of the square 'b' and the square of the sums 'a'
+print (b-a)
