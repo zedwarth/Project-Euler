71203bdbe8fafc34b3a5f3de1ec690c2f60ec9b2
zedwarth <zedwarth@gmail.com>
2012-08-09 20:22:27 -0400

    fixed line 14 error

--- problem-7/7.py
+++ problem-7/7.py
@@ -3,15 +3,17 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 #What is the 10001st prime number?
+
 #This is an implementation of the Sieve of Eratosthenes.
 
-
-p = [2, 3]
+p = [2, 3, 5]
 #p contains the list of found prime numbers
-n = 4 
+n = 7
 #n contains the current number being tested for primality
-while len(p) <=1000: #the loop run until the 10001th prime number is found, starting from 0
-    if all ([n % x != 0 for x in p]):
+while len(p) <=10000: #the loop run until the 10001th prime number is found, starting from 0
+    if n % 2 == 0 or n % 5 == 0:
+        n += 1
+    elif all ([n % x != 0 for x in p]):
 #test n for primality by trail division with all the currently know prime numbers
         p.append(n) #if n is found to be prime it is added to the list of prime number
         n+=1 #n increase by 1 to test the next number for primality
