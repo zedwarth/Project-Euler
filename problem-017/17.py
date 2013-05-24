2301a376438b3e76e76bad67beec275df9b380bb
zedwarth <zedwarth@gmail.com>
2013-05-23 21:05:57 -0400

    errors with whitespace

--- problem-017/17.py
+++ problem-017/17.py
@@ -3,9 +3,9 @@
 #
 numbers = { 0:'', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety', 100:'hundred'}
 
+total = 0
 def nwords(n):
     ''' Given an int n, from 1-1000, the fuction will reference the numbers dictionary and return a string of n written out as words
-        
         example:
         nwords(101)
         'onehundredandone'
@@ -21,5 +21,6 @@ def nwords(n):
         return (numbers[int(n[-4])] + 'thousand'
 
 for i in range(1,1001):
+    total += len(nwords(i))
 
-
+print(total)
